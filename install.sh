c9f3cd77c70628173198a3a7a9d5d10c2b678033
mitander <carlmitander@gmail.com>
2022-02-10 16:12:39 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,24 @@
+# Create temporary build location
+mkdir -p ~/tmp
+cd ~/tmp
+
+# Install dependancies
+sudo apt install zsh tmux build-essential libtool-bin gettext
+
+# Install neovim
+which nvim
+nvim -v
+git clone --depth 1 --branch nightly https://github.com/neovim/neovim.git
+cd ./neovim
+./configure --prefix=$HOME/.local/bin
+sudo make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+
+
+cd $HOME/dotfiles
+stow nvim
+stow zsh
+stow tmux
+
+# Remove build location
+cd
