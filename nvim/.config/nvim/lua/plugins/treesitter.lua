094b2fd9f06ea53f156b207067212286c30ee1f0
mitander <carlmitander@gmail.com>
2022-04-30 22:42:50 +0200

    Formatting

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,17 @@ if not status_ok then
 end
 
 configs.setup({
-	ensure_installed = "go", "zig", "rust", "lua", "vim", "python", "c", "c++", "javascript", "typescript",
+	ensure_installed = "go",
+	"zig",
+	"rust",
+	"lua",
+	"vim",
+	"python",
+	"c",
+	"c++",
+	"javascript",
+	"typescript",
+
 	sync_install = false,
 	ignore_install = { "" },
 	autopairs = {
