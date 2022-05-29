186c4a00b2bf83586913f1d0832fca637d737da9
mitander <carlmitander@gmail.com>
2022-05-29 23:05:30 +0200

    null-ls: clean up

--- nvim/.config/nvim/lua/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -5,10 +5,8 @@ end
 
 local formatting = null_ls.builtins.formatting
 
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
 null_ls.setup({
-	debug = true,
+	debug = false,
 	sources = {
 		formatting.clang_format,
 		formatting.gofmt,
@@ -17,18 +15,10 @@ null_ls.setup({
 		formatting.json_tool,
 		formatting.stylua,
 	},
-	on_attach = function(client, bufnr)
-            if client.supports_method("textDocument/formatting") then
-                print('wtf')
-                vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    group = augroup,
-                    buffer = bufnr,
-                    callback = function()
-                        vim.lsp.buf.format({ bufnr = bufnr })
-                    end,
-                })
-        end
+	on_attach = function(client)
+		if client.server_capabilities.document_formatting then
+			vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.format() async=true")
+		end
 	end,
 	offset_encoding = "utf-8",
 })
