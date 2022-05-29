5c4ec74e99801e55b9df5fecce792a401019cf41
mitander <carlmitander@gmail.com>
2022-05-29 23:04:24 +0200

    null-ls: format fix

--- nvim/.config/nvim/lua/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -1,22 +1,34 @@
 local ok, null_ls = pcall(require, "null-ls")
 if not ok then
-    return
+	return
 end
 
 local formatting = null_ls.builtins.formatting
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup({
-    debug = true,
-    sources = {
-        formatting.clang_format,
-        formatting.gofmt,
-        formatting.zigfmt,
-        formatting.rustfmt,
-        formatting.json_tool,
-        formatting.stylua,
-    },
-    on_attach = function()
-        vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.format() async=true")
-    end,
-    offset_encoding = "utf-8",
+	debug = true,
+	sources = {
+		formatting.clang_format,
+		formatting.gofmt,
+		formatting.zigfmt,
+		formatting.rustfmt,
+		formatting.json_tool,
+		formatting.stylua,
+	},
+	on_attach = function(client, bufnr)
+            if client.supports_method("textDocument/formatting") then
+                print('wtf')
+                vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    group = augroup,
+                    buffer = bufnr,
+                    callback = function()
+                        vim.lsp.buf.format({ bufnr = bufnr })
+                    end,
+                })
+        end
+	end,
+	offset_encoding = "utf-8",
 })
