982b8a7790d66e96ffd295961f74cb5ad4966809
mitander <carlmitander@gmail.com>
2022-05-29 23:26:08 +0200

    null-ls: check document_formatting

--- nvim/.config/nvim/lua/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -1,22 +1,24 @@
 local ok, null_ls = pcall(require, "null-ls")
 if not ok then
-	return
+    return
 end
 
 local formatting = null_ls.builtins.formatting
 
 null_ls.setup({
-	debug = false,
-	sources = {
-		formatting.clang_format,
-		formatting.gofmt,
-		formatting.zigfmt,
-		formatting.rustfmt,
-		formatting.json_tool,
-		formatting.stylua,
-	},
-	on_attach = function()
-		vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()")
-	end,
-	offset_encoding = "utf-8",
+    debug = false,
+    sources = {
+        formatting.clang_format,
+        formatting.gofmt,
+        formatting.zigfmt,
+        formatting.rustfmt,
+        formatting.json_tool,
+        formatting.stylua,
+    },
+    on_attach = function(client)
+        if client.server_capabilities.document_formatting then
+            vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()")
+        end
+    end,
+    offset_encoding = "utf-8",
 })
