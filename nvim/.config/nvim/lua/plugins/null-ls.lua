34f88a7de7448bb9bf8864909559affcbf3ab108
mitander <carlmitander@gmail.com>
2022-06-08 22:46:11 +0200

    Format: lua files

--- nvim/.config/nvim/lua/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -1,22 +1,31 @@
 local ok, null_ls = pcall(require, "null-ls")
 if not ok then
-    return
+	return
 end
 
 local formatting = null_ls.builtins.formatting
 
 null_ls.setup({
-    debug = true,
-    sources = {
-        formatting.clang_format,
-        formatting.gofmt,
-        formatting.zigfmt,
-        formatting.rustfmt,
-        formatting.json_tool,
-        formatting.stylua,
-    },
-    on_attach = function()
-        vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync(nil, 2000)")
-    end,
-    offset_encoding = "utf-8",
+	debug = true,
+	offset_encoding = "utf-8",
+	sources = {
+		formatting.clang_format,
+		formatting.gofmt,
+		formatting.zigfmt,
+		formatting.rustfmt,
+		formatting.json_tool,
+		formatting.stylua,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -1,107 +1,107 @@
 local nvim_tree_ok, nvim_tree = pcall(require, "nvim-tree")
 if not nvim_tree_ok then
-    return
+	return
 end
 
 local config_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
 if not config_ok then
-    return
+	return
 end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup({
-    renderer = {
-        icons = {
-            glyphs = {
-                default = "",
-                symlink = "",
-                git = {
-                    unstaged = "",
-                    staged = "S",
-                    unmerged = "",
-                    renamed = "➜",
-                    deleted = "",
-                    untracked = "U",
-                    ignored = "◌",
-                },
-                folder = {
-                    default = "",
-                    open = "",
-                    empty = "",
-                    empty_open = "",
-                    symlink = "",
-                },
-            },
-        },
-    },
-    disable_netrw = true,
-    hijack_netrw = true,
-    open_on_setup = false,
-    open_on_tab = true,
-    hijack_cursor = false,
-    update_cwd = true,
-    update_focused_file = {
-        enable = true,
-        update_cwd = true,
-    },
-    diagnostics = {
-        enable = true,
-        icons = {
-            hint = "",
-            info = "",
-            warning = "",
-            error = "",
-        },
-    },
-    system_open = {
-        cmd = nil,
-        args = {},
-    },
-    filters = {
-        dotfiles = false,
-        custom = {},
-    },
-    view = {
-        width = 50,
-        height = 30,
-        hide_root_folder = false,
-        side = "left",
-        auto_resize = true,
-        mappings = {
-            custom_only = false,
-            list = {
-                { key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
-                { key = { "h", "<BS>" }, cb = tree_cb("close_node") },
-                { key = "h", cb = tree_cb("close_node") },
-                { key = "<c-v>", cb = tree_cb("vsplit") },
-                { key = "<c-s>", cb = tree_cb("split") },
-                { key = "<c-t>", cb = tree_cb("tab") },
-            },
-        },
-        number = false,
-        relativenumber = false,
-    },
-    trash = {
-        cmd = "trash",
-        require_confirm = true,
-    },
-    actions = {
-        open_file = {
-            quit_on_open = false,
-        },
-    },
+	renderer = {
+		icons = {
+			glyphs = {
+				default = "",
+				symlink = "",
+				git = {
+					unstaged = "",
+					staged = "S",
+					unmerged = "",
+					renamed = "➜",
+					deleted = "",
+					untracked = "U",
+					ignored = "◌",
+				},
+				folder = {
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+				},
+			},
+		},
+	},
+	disable_netrw = true,
+	hijack_netrw = true,
+	open_on_setup = false,
+	open_on_tab = true,
+	hijack_cursor = false,
+	update_cwd = true,
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+	},
+	diagnostics = {
+		enable = true,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
+	system_open = {
+		cmd = nil,
+		args = {},
+	},
+	filters = {
+		dotfiles = false,
+		custom = {},
+	},
+	view = {
+		width = 50,
+		height = 30,
+		hide_root_folder = false,
+		side = "left",
+		auto_resize = true,
+		mappings = {
+			custom_only = false,
+			list = {
+				{ key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
+				{ key = { "h", "<BS>" }, cb = tree_cb("close_node") },
+				{ key = "h", cb = tree_cb("close_node") },
+				{ key = "<c-v>", cb = tree_cb("vsplit") },
+				{ key = "<c-s>", cb = tree_cb("split") },
+				{ key = "<c-t>", cb = tree_cb("tab") },
+			},
+		},
+		number = false,
+		relativenumber = false,
+	},
+	trash = {
+		cmd = "trash",
+		require_confirm = true,
+	},
+	actions = {
+		open_file = {
+			quit_on_open = false,
+		},
+	},
 })
 
 local util = require("plugins.util")
 local colors = require("plugins.colors")
 
 util.highlight({
-    { group = "NvimTreeSymlink", bg = colors.none, fg = colors.magneta },
-    { group = "NvimTreeFolderName", bg = colors.none, fg = colors.white },
-    { group = "NvimTreeRootFolder", bg = colors.none, fg = colors.magneta },
-    { group = "LspDiagnosticsError", bg = colors.none, fg = colors.red },
-    { group = "LspDiagnosticsWarning", bg = colors.none, fg = colors.yellow },
-    { group = "LspDiagnosticsInformation", bg = colors.none, fg = colors.white },
-    { group = "LspDiagnosticsHint", bg = colors.none, fg = colors.white },
+	{ group = "NvimTreeSymlink", bg = colors.none, fg = colors.magneta },
+	{ group = "NvimTreeFolderName", bg = colors.none, fg = colors.white },
+	{ group = "NvimTreeRootFolder", bg = colors.none, fg = colors.magneta },
+	{ group = "LspDiagnosticsError", bg = colors.none, fg = colors.red },
+	{ group = "LspDiagnosticsWarning", bg = colors.none, fg = colors.yellow },
+	{ group = "LspDiagnosticsInformation", bg = colors.none, fg = colors.white },
+	{ group = "LspDiagnosticsHint", bg = colors.none, fg = colors.white },
 })
