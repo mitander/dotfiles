0b62aaf23d0e7447d2e3d9ec91bcb6339773b547
mitander <carlmitander@gmail.com>
2022-07-06 15:10:25 +0200

    commentary: revert to space since leader is not working

--- nvim/.config/nvim/lua/plugins/mappings.lua
+++ nvim/.config/nvim/lua/plugins/mappings.lua
@@ -47,7 +47,7 @@ M.undotree = function()
 end
 
 M.commentary = function()
-	vim.cmd([[map <silent> <leader>/ :Commentary<enter>]])
+    vim.cmd([[map <silent> <space>/ :Commentary<enter>]])
 end
 
 M.bufferline = function()
