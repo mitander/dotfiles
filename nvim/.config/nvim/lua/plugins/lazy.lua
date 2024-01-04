2f2b68c79c21aeff4f7046eeb851df2e39cb7d82
mitander <carlmitander@gmail.com>
2024-01-04 23:54:50 +0100

    colors: indent

--- nvim/.config/nvim/lua/plugins/lazy.lua
+++ nvim/.config/nvim/lua/plugins/lazy.lua
@@ -16,6 +16,7 @@ local plugins = {
             vim.cmd [[hi StatusLineNC guibg=#363646]]
             vim.cmd [[hi LineNr guifg=NONE]]
             vim.cmd [[hi Todo guifg=#E6C384 guibg=#1F1F28]]
+            vim.cmd [[hi IblIndent guifg=#35353d guibg=NONE]]
         end,
     },
 
