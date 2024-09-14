71658a091043abc97ed227a76acece50e0ee3ffd
mitander <carlmitander@gmail.com>
2024-09-14 12:13:06 +0200

    neotest: update

--- nvim/.config/nvim/lua/mitander/lazy/neotest.lua
+++ nvim/.config/nvim/lua/mitander/lazy/neotest.lua
@@ -6,12 +6,15 @@ return {
         "nvim-treesitter/nvim-treesitter",
         "marilari88/neotest-vitest",
         "nvim-neotest/neotest-plenary",
+        "lawrence-laz/neotest-zig",
+        "nvim-neotest/nvim-nio"
     },
     config = function()
         local neotest = require("neotest")
         neotest.setup({
             adapters = {
                 require("neotest-vitest"),
+                require("neotest-zig"),
                 require("neotest-plenary").setup({ min_init = "./scripts/tests/minimal.vim" }),
             }
         })
