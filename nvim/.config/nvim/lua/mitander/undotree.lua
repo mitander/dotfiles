5db2852643496ddc66c6009cce2c9b03a2b03168
mitander <carlmitander@gmail.com>
2025-11-09 00:07:28 +0100

    undotree: cleaner view

--- nvim/.config/nvim/lua/mitander/undotree.lua
+++ nvim/.config/nvim/lua/mitander/undotree.lua
@@ -1,6 +1,16 @@
 return {
     "mbbill/undotree",
-    config = function()
-        vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+    cmd = { "UndotreeToggle", "UndotreeShow", "UndotreeHide" },
+    keys = {
+        { "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "Toggle undo tree" },
+    },
+    init = function()
+        vim.g.undotree_WindowLayout = 2
+        vim.g.undotree_SplitWidth = 40
+        vim.g.undotree_SetFocusWhenToggle = 1
+        vim.g.undotree_ShortIndicators = 1
+        vim.g.undotree_DiffAutoOpen = 1
+        vim.g.undotree_DiffpanelHeight = 10
+        vim.g.undotree_HelpLine = 0
     end,
 }
