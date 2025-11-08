59a7077ab07b718433bd5890770880e5d16d9bf3
mitander <carlmitander@gmail.com>
2025-11-08 23:59:42 +0100

    nvim: use Comment.nvim
    sorry tpope

--- nvim/.config/nvim/lua/mitander/commentary.lua
+++ nvim/.config/nvim/lua/mitander/commentary.lua
@@ -1,7 +1,27 @@
 return {
-    event = "BufReadPre",
-    "tpope/vim-commentary",
-    init = function()
-        vim.cmd([[map <silent> <leader>/ :Commentary<enter>]])
+    "numToStr/Comment.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+        require("Comment").setup({
+            padding = true,
+            sticky = true,
+            ignore = "^$",
+            toggler = {
+                line = "gcc",
+                block = "gbc",
+            },
+            opleader = {
+                line = "gc",
+                block = "gb",
+            },
+            mappings = {
+                basic = true,
+                extra = false,
+            },
+        })
+
+        -- Preserve <leader>/ keymap for commenting
+        vim.keymap.set("n", "<leader>/", "gcc", { remap = true, silent = true })
+        vim.keymap.set("v", "<leader>/", "gc", { remap = true, silent = true })
     end,
 }
